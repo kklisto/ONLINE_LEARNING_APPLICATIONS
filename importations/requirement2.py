4195c0494c3f418c0870f7078e77e302fd92b6e8
kklisto <kwillaey@gmail.com>
2024-12-11 12:05:56 +0100

    progress

--- importations/requirement2.py
+++ importations/requirement2.py
@@ -83,4 +83,38 @@ class FFMultiplicativePacingAgent:
         # update budget
         self.budget -= c_t
 
+class PricingEnvironment:
+    def __init__(self, conversion_probability, cost):
+        self.conversion_probability = conversion_probability
+        self.cost = cost
+
+    def round(self, p_t, n_t):
+        d_t = np.random.binomial(n_t, self.conversion_probability(p_t))
+        r_t = (p_t - self.cost)*d_t
+        return d_t, r_t
+    
+class UCB1Agent:
+    def __init__(self, K, T, range=1):
+        self.K = K
+        self.T = T
+        self.range = range
+        self.a_t = None
+        self.average_rewards = np.zeros(K)
+        self.N_pulls = np.zeros(K)
+        self.t = 0
+    
+    def pull_arm(self):
+        if self.t < self.K:
+            self.a_t = self.t 
+        else:
+            ucbs = self.average_rewards + self.range*np.sqrt(2*np.log(self.T)/self.N_pulls)
+            self.a_t = np.argmax(ucbs)
+        return self.a_t
+    
+    def update(self, r_t):
+        self.N_pulls[self.a_t] += 1
+        self.average_rewards[self.a_t] += (r_t - self.average_rewards[self.a_t])/self.N_pulls[self.a_t]
+        self.t += 1
+        
+
         
